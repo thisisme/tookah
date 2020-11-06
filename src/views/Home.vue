9808202f4fa7842251dbd745c0f9236b1be2f94a
Mats Olsberg <mats.olsberg@gmail.com>
2020-11-06 11:53:41 +0100

    Add timers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,28 @@
     <h1>Tookah!</h1>
     <section class="game">
       <div class="game__stats">
-        <div class="game__stats--stat timer">20 seconds left</div>
+        <div class="game__stats--stat timer">{{ questionTimer }} seconds left</div>
         <div class="game__stats--stat correct__answers">Correct answers: {{ correctAnswers }}</div>
         <div class="game__stats--stat current__question">Current question: {{ currentQuestion }} / 10</div>
       </div>
+      <div
+        v-if="showQuestion === false"
+        class="game__prepare">
+        <h3>Prepare for next question</h3>
+        <h2>{{ prepareTimer }}</h2>
+      </div>
       <Question
+        v-if="showQuestion"
         :question="getCurrentQuestion()"
         @answer="setAnswer" />
+      <button @click="setPrepareTimer">
+        <template v-if="currentQuestion === 1">
+          Start
+        </template>
+        <template v-else>
+          Next question
+        </template>
+      </button>
     </section>
   </div>
 </template>
@@ -25,7 +40,11 @@ export default {
   data () {
     return {
       correctAnswers: 1,
-      currentQuestion: 1
+      currentQuestion: 1,
+      showQuestion: false,
+      questionTimer: 15,
+      prepareTimer: 5,
+      timer: undefined
     }
   },
   computed: {
@@ -39,6 +58,26 @@ export default {
     },
     setAnswer (answer) {
       console.log(answer)
+      clearInterval(this.timer)
+    },
+    setQuestionTimer () {
+      this.timer = setInterval(() => {
+        this.questionTimer -= 1
+        if (this.questionTimer === 0) {
+          clearInterval(this.timer)
+        }
+      }, 1000)
+    },
+    setPrepareTimer () {
+      this.timer = setInterval(() => {
+        this.prepareTimer -= 1
+        if (this.prepareTimer === 0) {
+          clearInterval(this.timer)
+          this.setQuestionTimer()
+          this.getCurrentQuestion()
+          this.showQuestion = true
+        }
+      }, 1000)
     }
   }
 }
