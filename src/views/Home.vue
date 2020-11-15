5df62202d5d33000d799fad3003b97b08421aed8
Mats Olsberg <mats.olsberg@gmail.com>
2020-11-15 17:00:23 +0100

    Fix timers, add screen between states, control buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,32 +12,56 @@
         </div>
       </div>
       <div
+        class="game__answer"
+        v-if="correctAnswer != undefined">
+        <h2>
+          <span v-if="correctAnswer === true">Great! Correct answer</span>
+          <span v-else>Sorry! Wrong answer</span>!
+        </h2>
+      </div>
+      <div
         v-if="showQuestion === false"
         class="game__prepare">
-        <h3>Prepare for next question</h3>
-        <h2>{{ prepareTimer }}</h2>
+        <template v-if="!lastAnswered">
+          <h3>Prepare for next question</h3>
+          <h2>{{ prepareTimer }}</h2>
+        </template>
+        <template v-else>
+          <h2>Game over!</h2>
+        </template>
       </div>
       <Question
         v-if="showQuestion"
         :question="getCurrentQuestion()"
         @answer="setAnswer" />
-      <button
-        @click="setPrepareTimer"
-        :disabled="questionLoaded === false">
-        <template v-if="currentQuestion === 1">
-          Start
-        </template>
-        <template v-else>
-          Next question
-        </template>
-      </button>
+      <div
+        class="game__progress"
+        v-if="!lastAnswered">
+        <button
+          @click="setPrepareTimer"
+          :disabled="showQuestion === true">
+          <template v-if="currentQuestion === 1">
+            Start
+          </template>
+          <template v-else>
+            Next question
+          </template>
+        </button>
+      </div>
       <div class="game__lifelines">
-        <button>
-          50/50
-        </button>
-        <button>
-          + 10 seconds
-        </button>
+        <h3>Lifelines</h3>
+        <p>You can use each lifeline one time only.</p>
+        <p>50/50 removes two options and + 10 will give you 10 extra seconds</p>
+        <div class="lifelines">
+          <button
+            @click="lifelineHalf">
+            50/50
+          </button>
+          <button
+           @click="lifelineTime">
+            + 10 seconds
+          </button>
+        </div>
       </div>
     </section>
   </div>
@@ -53,8 +77,10 @@ export default {
   },
   data () {
     return {
+      correctAnswer: undefined,
       correctAnswers: 0,
       currentQuestion: 0,
+      lastAnswered: false,
       totalQuestions: 0,
       showQuestion: false,
       questionTimer: 15,
@@ -69,21 +95,45 @@ export default {
     }
   },
   methods: {
+    /**
+     * Get current question
+     * Returns object with question
+     */
     getCurrentQuestion () {
       return this.questions[this.currentQuestion - 1]
     },
-    setAnswer (answer) {
-      if (answer === true) {
-        this.correctAnswers += 1
+    /**
+     * Show current question
+     */
+    question () {
+      if (this.currentQuestion <= this.questions.length) {
+        this.showQuestion = true
+        this.setQuestionTimer()
+        this.correctAnswer = undefined
       }
+    },
+    /**
+     * Set answer
+     */
+    setAnswer (answer) {
       clearInterval(this.timer)
       this.showQuestion = false
+      this.correctAnswer = false
+      if (answer === true) {
+        this.correctAnswer = true
+        this.correctAnswers += 1
+      }
       if (this.currentQuestion < this.questions.length) {
         this.currentQuestion += 1
         this.questionTimer = 15
         this.prepareTimer = 5
+      } else {
+        this.lastAnswered = true
       }
     },
+    /**
+     * Set question timer
+     */
     setQuestionTimer () {
       this.timer = setInterval(() => {
         this.questionTimer -= 1
@@ -92,19 +142,29 @@ export default {
         }
       }, 1000)
     },
+    /**
+     * Set prepare timer
+     */
     setPrepareTimer () {
       this.timer = setInterval(() => {
         this.prepareTimer -= 1
         if (this.prepareTimer === 0) {
           clearInterval(this.timer)
-          this.setQuestionTimer()
-          this.getCurrentQuestion()
-          this.showQuestion = true
+          this.question()
         }
       }, 1000)
+    },
+    lifelineHalf () {
+      console.log('remove to options')
+    },
+    lifelineTime () {
+      this.timer += 10
     }
   },
   mounted () {
+    /**
+     * Fetch questions from api
+     */
     this.$store.dispatch('questions/fetchQuestions').then(() => {
       this.questionLoaded = true
       this.currentQuestion = 1
