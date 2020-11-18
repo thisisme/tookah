da920e76179802cc7ac97542488940ad7a32a841
Mats Olsberg <mats.olsberg@gmail.com>
2020-11-18 11:32:02 +0100

    Add check for elemnts on first page

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Check for header', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.contains('h1', 'Tookah!')
   })
 })
