f0b6a787674813d8b4de239bce3edb21d38175f9
Mats Olsberg <mats.olsberg@gmail.com>
2020-11-18 16:31:46 +0100

    Add uuid and apply to questions

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import { createServer, Model } from 'miragejs'
+import { v4 as uuidv4 } from 'uuid'
 
 export function makeServer ({ environment = 'development' } = {}) {
   const server = createServer({
@@ -10,72 +11,79 @@ export function makeServer ({ environment = 'development' } = {}) {
 
     seeds (server) {
       server.create('question', {
+        id: uuidv4(),
         text: 'What year was Pulp Fiction released?',
         alternatives: [
-          { text: '2004', id: 1, answer: false },
-          { text: '2000', id: 2, answer: false },
-          { text: '1994', id: 3, answer: true },
-          { text: '1997', id: 4, answer: false }
+          { text: '2004', id: uuidv4(), answer: false },
+          { text: '2000', id: uuidv4(), answer: false },
+          { text: '1994', id: uuidv4(), answer: true },
+          { text: '1997', id: uuidv4(), answer: false }
         ],
         used: false
       })
       server.create('question', {
+        id: uuidv4(),
         text: 'Who produced The Grand Budapest Hotel?',
         alternatives: [
-          { text: 'Steven Spielberg', id: 5, answer: false },
-          { text: 'Mel Brooks', id: 6, answer: false },
-          { text: 'Martin Scorsese', id: 7, answer: false },
-          { text: 'Wes Anderson', id: 8, answer: true }
+          { text: 'Steven Spielberg', id: uuidv4(), answer: false },
+          { text: 'Mel Brooks', id: uuidv4(), answer: false },
+          { text: 'Martin Scorsese', id: uuidv4(), answer: false },
+          { text: 'Wes Anderson', id: uuidv4(), answer: true }
         ],
         used: false
       })
       server.create('question', {
+        id: uuidv4(),
         text: 'In Big Lebowski there is a swedish actor, who?',
         alternatives: [
-          { text: 'Peter Stormare', id: 5, answer: true },
-          { text: 'Max von Sydow', id: 6, answer: false },
-          { text: 'Stellan Skarsgård', id: 7, answer: false },
-          { text: 'Dolph Lundgren', id: 8, answer: false }
+          { text: 'Peter Stormare', id: uuidv4(), answer: true },
+          { text: 'Max von Sydow', id: uuidv4(), answer: false },
+          { text: 'Stellan Skarsgård', id: uuidv4(), answer: false },
+          { text: 'Dolph Lundgren', id: uuidv4(), answer: false }
         ],
         used: false
       })
       server.create('question', {
+        id: uuidv4(),
         text: 'Who wrote Fear and Loathing in Las Vegas?',
         alternatives: [
-          { text: 'Terry Gilliam', id: 5, answer: false },
-          { text: 'Hunter s. Thompson', id: 6, answer: true },
-          { text: 'Stanley Kubrick', id: 7, answer: false },
-          { text: 'Spike Jonze', id: 8, answer: false }
+          { text: 'Terry Gilliam', id: uuidv4(), answer: false },
+          { text: 'Hunter s. Thompson', id: uuidv4(), answer: true },
+          { text: 'Stanley Kubrick', id: uuidv4(), answer: false },
+          { text: 'Spike Jonze', id: uuidv4(), answer: false }
         ],
         used: false
       })
       server.create('question', {
+        id: uuidv4(),
         text: 'Who the soundtrack to The Social Network?',
         alternatives: [
-          { text: 'David Bowie', id: 5, answer: false },
-          { text: 'Prince', id: 6, answer: true },
-          { text: 'Trent Reznor', id: 7, answer: true },
-          { text: 'Nick Cave', id: 8, answer: false }
+          { text: 'David Bowie', id: uuidv4(), answer: false },
+          { text: 'Prince', id: uuidv4(), answer: true },
+          { text: 'Trent Reznor', id: uuidv4(), answer: true },
+          { text: 'Nick Cave', id: uuidv4(), answer: false }
         ],
         used: false
       })
       server.create('question', {
+        id: uuidv4(),
         text: 'Who played Batman in Batman Begins?',
         alternatives: [
-          { text: 'Christian Bale', id: 5, answer: true },
-          { text: 'Robert Pattinson', id: 6, answer: false },
-          { text: 'Michael Keaton', id: 7, answer: true },
-          { text: 'Adam West', id: 8, answer: false }
+          { text: 'Christian Bale', id: uuidv4(), answer: true },
+          { text: 'Robert Pattinson', id: uuidv4(), answer: false },
+          { text: 'Michael Keaton', id: uuidv4(), answer: true },
+          { text: 'Adam West', id: uuidv4(), answer: false }
         ],
         used: false
       })
       server.create('question', {
+        id: uuidv4(),
         text: 'When was Jojo Rabbit released?',
         alternatives: [
-          { text: '2009', id: 5, answer: false },
-          { text: '2019', id: 6, answer: true },
-          { text: '2015', id: 7, answer: true },
-          { text: '2012', id: 8, answer: false }
+          { text: '2009', id: uuidv4(), answer: false },
+          { text: '2019', id: uuidv4(), answer: true },
+          { text: '2015', id: uuidv4(), answer: true },
+          { text: '2012', id: uuidv4(), answer: false }
         ],
         used: false
       })
